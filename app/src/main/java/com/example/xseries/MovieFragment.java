35b5a4c4a88abc15a7a7793f4b1e22b58781cc63
Abhi <avvjit@gmail.com>
2023-05-05 15:48:07 +0530

    Collection tab layout implement

--- app/src/main/java/com/example/xseries/MovieFragment.java
+++ app/src/main/java/com/example/xseries/MovieFragment.java
@@ -3,23 +3,17 @@ package com.example.xseries;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.material.tabs.TabLayout;
-
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CollectionFragment#newInstance} factory method to
+ * Use the {@link MovieFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CollectionFragment extends Fragment {
-
-    TabLayout tabLayout;
-    ViewPager viewPager;
+public class MovieFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,7 +24,7 @@ public class CollectionFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public CollectionFragment() {
+    public MovieFragment() {
         // Required empty public constructor
     }
 
@@ -40,11 +34,11 @@ public class CollectionFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment CollectionFragment.
+     * @return A new instance of fragment MovieFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CollectionFragment newInstance(String param1, String param2) {
-        CollectionFragment fragment = new CollectionFragment();
+    public static MovieFragment newInstance(String param1, String param2) {
+        MovieFragment fragment = new MovieFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,20 +59,6 @@ public class CollectionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_collection, container, false);
-        addFragment(view);
-        return view;
-    }
-
-    private void addFragment(View view) {
-
-        tabLayout = view.findViewById(R.id.tabLayout);
-        viewPager = view.findViewById(R.id.viewPager);
-
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
-        adapter.addFragment(new ShowFragment(), "Shows");
-        adapter.addFragment(new MovieFragment(), "Movies");
-        viewPager.setAdapter(adapter);
-        tabLayout.setupWithViewPager(viewPager);
+        return inflater.inflate(R.layout.fragment_movie, container, false);
     }
 }
